1dbb56d76d2c954b5be3b57fb6289bf7a8677877
Thomas Boch <thomas.boch@astro.unistra.fr>
2024-03-18 14:56:43 +0100

    Add LGPL3 header

--- hips2fits_cutout.py
+++ hips2fits_cutout.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
+ ##############################################################################
+ #
+ # Copyright 2024 - Thomas Boch
+ #
+ # This file is part of HiPS2FITS cutout script.
+ #
+ # HiPS2FITS cutout script is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU Lesser General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # HiPS2FITS cutout script is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU Lesser General Public License for more details.
+ #
+ # You should have received a copy of the GNU Lesser General Public License
+ # along with HiPS2FITS cutout script.  If not, see <http://www.gnu.org/licenses/>.
+ #
+ ##############################################################################
+
+
 import sys
 import os
 
